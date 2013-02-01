0766b6ea28203730d11fa55a6f870a5aa62e4e6c
Harley Trung <harley@socialsci.com>
2013-02-01 14:40:20 +0700

    nested resource 'table'

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 var App = Ember.Application.create();
 
 App.Router.map(function(){
-  this.resource('tables');
+  this.resource('tables', function() {
+    this.resource('table', {path: ':table_id'});
+  });
 });
 
 App.TablesRoute = Ember.Route.extend({
   model: function() {
     return App.Table.find();
+  }
+});
+
+App.TableRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.Table.find(params.table_id);
   }
 });
 
@@ -19,6 +27,7 @@ App.Store = DS.Store.extend({
 App.Table = DS.Model.extend();
 
 App.TableController = Ember.ArrayController.extend();
+App.TableController = Ember.ObjectController.extend();
 
 
 App.Table.FIXTURES = [
