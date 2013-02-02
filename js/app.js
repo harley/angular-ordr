2a4b317beadb9217ad1939cb42bb7b33c5a65d59
Harley Trung <harley@socialsci.com>
2013-02-02 18:47:03 +0700

    rewrite app in angular

--- js/app.js
+++ js/app.js
@@ -1,147 +1,88 @@
-var App = Ember.Application.create();
+var app = angular.module('ordr', ['filters']);
 
-App.Router.map(function(){
-  this.resource('tables', function() {
-    this.resource('table', {path: ':table_id'});
-  });
-});
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/tables', {
+    templateUrl: 'partials/tables.html', 
+    controller: 'TablesCtrl'
+  }).
+  when('/tables/:tableId', {
+    templateUrl: 'partials/table.html', 
+    controller: 'TableCtrl'
+  }).
+  otherwise({redirectTo: '/tables'});
+}]);
 
-App.ApplicationRoute = Ember.Route.extend({
-  setupController: function(){
-    this.controllerFor('food').set('model', App.Food.find());
-    App.Food.find();
+angular.module('filters', []).filter('money', function(){
+  return function(value) {
+    if (isNaN(value)) { return "0.00"; }
+    return '$' + (value % 100 === 0 ?
+                 (value / 100 + ".00") : 
+                 (parseInt(value/100, 10) + '.' + value % 100));
   }
 });
 
-App.IndexRoute = Ember.Route.extend({
-  redirect: function(){
-    this.transitionTo('tables');
+app.controller("AppCtrl", function ($scope) {
+  $scope.foods = FIXTURES.Food;
+  $scope.totalCents = function(tabItems) {
+    console.log("totalCents on ", tabItems)
+    var total = 0;
+    for (var i = 0; i < tabItems.length; i++) {
+      total += tabItems[i].cents;
+    }
+    return total;
   }
 });
 
-App.TablesRoute = Ember.Route.extend({
-  model: function() {
-    return App.Table.find();
-  }
+app.controller("TablesCtrl", function ($scope) {
+  $scope.tables = FIXTURES.Table;
+  $scope.tablePage = "partials/table.html";
 });
 
-App.TablesController = Ember.ArrayController.extend({
-  sortProperties: ['id']
-});
+app.controller("TableCtrl",  function ($scope, $routeParams) {
+  $scope.id = $routeParams.tableId;
 
-App.FoodController = Ember.ArrayController.extend({
-  addFood: function(food) {
-    var table = this.controllerFor('table').get('model'),
-        tabItems = table.get('tab.tabItems');
-    tabItems.createRecord({
+  // load table
+  $scope.table = _.find(FIXTURES.Table, function(e) { return String(e.id) === $scope.id; });
+  console.log($scope.table);
+
+  // load tab
+  $scope.tab = _.find(FIXTURES.Tab, function(e) { return e.id === $scope.table.tab });
+  console.log($scope.table.tab);
+
+  // load tab items
+  // this is a very stupid way to map record ids to records. but works for now
+  $scope.tab.tabItems = _.map($scope.tab.tabItems, function(tabItemId){
+    var tabItem = _.find(FIXTURES.TabItem, function(e){
+      return tabItemId === e.id || tabItemId === e;
+    });
+    if (tabItem) {
+      tabItem.food = _.find(FIXTURES.Food, function(e){
+        return tabItem.food === e.id || tabItem.food === e; 
+      });
+    }
+    return tabItem;
+  })
+
+  // add tab item
+  $scope.addFood = function(food) {
+    console.log("tab: ", $scope.tab.tabItems);
+    $scope.tab.tabItems.push(
+    {
       food: food,
-      cents: food.get('cents')
+      cents: food.cents
     });
   }
-});
-// App.TabController = Ember.ObjectController.extend();
+})
 
-// View heleprs
-Ember.Handlebars.registerBoundHelper('money', function(value){
-  if (isNaN(value)) { return "0.00"; }
-  return (value % 100 === 0 ?
-          (value / 100 + ".00") : 
-          (parseInt(value/100, 10) + '.' + value % 100));
-});
+app.controller("FoodCtrl", function($scope) {
 
-// Models
-App.Store = DS.Store.extend({
-  revision: 11,
-  adapter: 'DS.FixtureAdapter'
-});
+})
 
-App.Table = DS.Model.extend({
-  tab: DS.belongsTo('App.Tab')
-});
+app.controller("TabCtrl", function($scope) {
+})
 
-App.Tab = DS.Model.extend({
-  tabItems: DS.hasMany('App.TabItem'),
-  cents: function(){
-    return this.get('tabItems').getEach('cents').reduce(function(acc, item){
-      return acc + item;
-    }, 0);
-  }.property('tabItems.@each.cents')
-});
-
-App.TabItem = DS.Model.extend({
-  cents: DS.attr('number'),
-  food: DS.belongsTo('App.Food')
-});
-
-App.Food = DS.Model.extend({
-  name: DS.attr('string'),
-  imageUrl: DS.attr('string'),
-  cents: DS.attr('number')
-});
-
-App.Table.FIXTURES = [{
-  id: 1,
-  tab: 1
-}, {
-  id: 2,
-  tab: 2
-}, {
-  id: 3,
-  tab: 3
-}, {
-  id: 4,
-  tab: 4
-}, {
-  id: 5,
-  tab: 5
-}, {
-  id: 6,
-  tab: 6
-}];
-
-App.Tab.FIXTURES = [{
-  id: 1,
-  tabItems: []
-}, {
-  id: 2,
-  tabItems: []
-}, {
-  id: 3,
-  tabItems: []
-}, {
-  id: 4,
-  tabItems: [400, 401, 402, 403, 404]
-}, {
-  id: 5,
-  tabItems: []
-}, {
-  id: 6,
-  tabItems: []
-}];
-
-App.TabItem.FIXTURES = [{
-  id: 400,
-  cents: 1500,
-  food: 1
-}, {
-  id: 401,
-  cents: 300,
-  food: 2
-}, {
-  id: 402,
-  cents: 700,
-  food: 3
-}, {
-  id: 403,
-  cents: 950,
-  food: 4
-}, {
-  id: 404,
-  cents: 2000,
-  food: 5
-}];
-
-App.Food.FIXTURES = [{
+FIXTURES = {}
+FIXTURES.Food = [{
   id: 1,
   name: 'Pizza',
   imageUrl: 'img/pizza.png',
@@ -166,4 +107,68 @@ App.Food.FIXTURES = [{
   name: 'Birthday Cake',
   imageUrl: 'img/birthdaycake.png',
   cents: 2000
+}];
+
+
+FIXTURES.TabItem = [{
+  id: 400,
+  cents: 1500,
+  food: 1
+}, {
+  id: 401,
+  cents: 300,
+  food: 2
+}, {
+  id: 402,
+  cents: 700,
+  food: 3
+}, {
+  id: 403,
+  cents: 950,
+  food: 4
+}, {
+  id: 404,
+  cents: 2000,
+  food: 5
+}];
+
+FIXTURES.Tab = [{
+  id: 1,
+  tabItems: []
+}, {
+  id: 2,
+  tabItems: []
+}, {
+  id: 3,
+  tabItems: []
+}, {
+  id: 4,
+  tabItems: [400, 401, 402, 403, 404]
+}, {
+  id: 5,
+  tabItems: []
+}, {
+  id: 6,
+  tabItems: []
+}];
+
+
+FIXTURES.Table = [{
+  id: 1,
+  tab: 1
+}, {
+  id: 2,
+  tab: 2
+}, {
+  id: 3,
+  tab: 3
+}, {
+  id: 4,
+  tab: 4
+}, {
+  id: 5,
+  tab: 5
+}, {
+  id: 6,
+  tab: 6
 }];
